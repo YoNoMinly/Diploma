4e7cd4ca02db19f4d4fcfc4c29f0d6bf0e9111ff
YoNoMinly <107247703+YoNoMinly@users.noreply.github.com>
2022-06-10 17:42:39 +0300

    Add files via upload

---
+++ Diploma/app/src/main/java/com/example/diploma/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.diploma;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    String[] items;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void runtimePermission()
+    {
+Dexter.withContext
+    }
+}
